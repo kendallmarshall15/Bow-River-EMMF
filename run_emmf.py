03037102190939624598dae62924694089b2ae51
kendallmarshall15 <kendall.marshall@ucalgary.ca>
2026-07-06 13:22:49 -0600

    Update run_emmf.py

--- run_emmf.py
+++ run_emmf.py
@@ -1,1 +1,20 @@
+from config.config import *
+from scripts.io import load_river, load_endmembers
+from scripts.preprocess import filter_site
+from scripts.monte_carlo import run_mc
+from scripts.export import export_results
 
+# Load data
+river = load_river(river_path, IONS_MODEL, META_COLS)
+em = load_endmembers(em_path, IONS_MODEL, END_MEMBER_COLS)
+
+# Filter
+river = filter_site(river)
+
+# Run model
+frac_draws, rmse_draws = run_mc(river, em, N_MC)
+
+# Export
+export_results(river, frac_draws, rmse_draws, out_path)
+
+print("EMMA run complete.")
